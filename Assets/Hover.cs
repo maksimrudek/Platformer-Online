ede9cbd7b7351df20aed191bc53ef08f705d1b2e
Maksim Rudek <maksim.rudek@gmail.com>
2023-04-12 16:41:51 +0300

    Hover

---
+++ Assets/Hover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float height = 0.1f;
+    public float speed = 10;
+    public float offset;
+
+    private void Start()
+    {
+        offset = Random.Range(0f, 100f);
+    }
+
+    private void Update()
+    {
+        transform.localPosition = Vector3.up * Mathf.Sin(offset + Time.time * speed * 6.28f) * height;
+    }
+}
